647b56279ba2ae91f6a0be8755fa269aa62418b7
Hesam Farjad <81914229+HesamFarjad@users.noreply.github.com>
2024-04-23 11:40:02 +0200

    Update data.py

--- data.py
+++ data.py
@@ -10,9 +10,4 @@ response = requests.get(url="https://opentdb.com/api.php", params=PARAMETERS)
 response.raise_for_status()
 data = response.json()
 
-# question_data = pandas.DataFrame(data['results'])
 question_data = data['results']
-# print(question_data)
-
-# questions = df['question']
-# print(questions)
